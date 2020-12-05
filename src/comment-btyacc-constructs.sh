23b1c3b1f4f32ae5b24e1bb91999d09bf609c010
Satya Das <SatyaRanjan.Das@tomtom.com>
2020-12-05 13:09:44 +0100

    minor changes

--- src/comment-btyacc-constructs.sh
+++ src/comment-btyacc-constructs.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-GRAMMER_START_LINE=371
-GRAMMER_END_LINE=1764
+########################################################################################
+## Comments out btyacc cnstructs so that bison/yacc can be used to generate grammer
+## It is only meant for diagnostic purpose to improve the grammer
+## and performance by reducing reduce/reduce conflicts
+########################################################################################
+
+GRAMMER_START_LINE=`grep -n '%%' parser.y | head -1 | cut -d":" -f1`
+GRAMMER_END_LINE=`grep -n '%%' parser.y | tail -1 | cut -d":" -f1`
 
 sed -i                                                                                                    \
   -e "${GRAMMER_START_LINE},${GRAMMER_END_LINE}s/\(\[[^'].*;[ \t]*]\)/\{\/*ToYacc & ToYacc*\/\}/g"        \
--- src/uncomment-btyacc-constructs.sh
+++ src/uncomment-btyacc-constructs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-GRAMMER_START_LINE=371
-GRAMMER_END_LINE=1764
+GRAMMER_START_LINE=`grep -n '%%' parser.y | head -1 | cut -d":" -f1`
+GRAMMER_END_LINE=`grep -n '%%' parser.y | tail -1 | cut -d":" -f1`
 
 sed -i                                                                   \
   -e "${GRAMMER_START_LINE},${GRAMMER_END_LINE}s/{\/\*ToYacc //g"        \
